3f003b54ff42d42aea627809b949df7072ea076e
Samuel Simko <sam@st14s.home>
2021-04-26 18:25:40 +0200

    docs: add scarf module as mock

--- docs/conf.py
+++ docs/conf.py
@@ -12,11 +12,13 @@
 #
 import os
 import sys
+import mock
 
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath("./.."))
-sys.path.insert(0, os.path.abspath("./../.."))
+sys.path.insert(0, os.path.abspath(".."))
+
+sys.modules['scarf'] = mock.Mock()
 
 
 # -- Project information -----------------------------------------------------
